332535784e81673581f81f9a17915a90cb1f188f
Michael Miles <michael.gage.miles@gmail.com>
2019-04-24 21:52:44 -0700

    Initial Commit

---
+++ SearchBarr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchBarr
+//
+//  Created by Michael Miles on 4/24/19.
+//  Copyright © 2019 Michael Miles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
